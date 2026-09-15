d517743d990f18e01279c7e97991b98364ff8447
Fahad <fahadkhan64525@gmail.com>
2026-09-15 20:37:44 +0530

    Fix background image URL syntax in HeroScene

--- src/components/hero/HeroScene.tsx
+++ src/components/hero/HeroScene.tsx
@@ -53,8 +53,7 @@ export default function HeroScene() {
         className="absolute inset-0 bg-cover bg-center will-change-transform"
         style={{
           backgroundImage:
-            "url('/images/projects/IMG-20260914-WA0003.jpg
-')",
+            "url('/images/projects/IMG-20260914-WA0003.jpg')",
           transform: "scale(1.15)",
         }}
       />
